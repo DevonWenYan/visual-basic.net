b6353757f0dee4e91243a542743cb7d9464c8d22
DevonWenYan <devonproverb@gmail.com>
2016-11-09 12:29:36 +0800

    Enum teaching
    Enum teaching

---
+++ Enum teaching/ConsoleApplication1/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+    Enum State : int
+        Binary = 2
+        Octal = 8
+        Hex = 16
+    End Enum
+
+    Sub Main()
+        Console.WriteLine("二進位的常數值是{0}", CType(State.Binary, Integer))
+        Console.WriteLine("八進位的常數值是{0}", CType(State.Octal, Integer))
+        Console.WriteLine("十六進位的常數值是{0}", CType(State.Hex, Integer))
+        Console.Read()
+
+    End Sub
+
+End Module
