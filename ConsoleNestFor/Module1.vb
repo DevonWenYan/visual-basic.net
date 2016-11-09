a46d7060dcd3f197b4d234c12d97b8493e82587e
DevonWenYan <devonproverb@gmail.com>
2016-11-09 16:40:54 +0800

    ConsoleNestFor
    ConsoleNestFor

---
+++ ConsoleNestFor/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, k As Integer   ' 宣告 i 為外層迴圈的初值，k 為內層迴圈的初值
+        For i = 1 To 5           ' 外層迴圈
+            For k = 1 To i      ' 內層迴圈
+                Console.Write("  {0}", k.ToString())  ' 游標停在同一列     
+            Next
+            Console.WriteLine()        ' 強迫換列
+        Next
+        Console.Read()
+    End Sub
+
+End Module
