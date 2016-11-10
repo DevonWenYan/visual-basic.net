685d6ad90cb3fc1fff93edee24727ca9727d19e6
DevonWenYan <devonproverb@gmail.com>
2016-11-10 16:49:41 +0800

    Prime number
    Prime number

---
+++ Prime number/ConsoleApplication3/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num, i, j, count As Integer
+        For i = 2 To 1000
+            count = 0
+            For j = 2 To i
+                num = i Mod j
+                If (num = 0) Then
+                    count += 1
+                End If
+            Next
+            If (count = 1) Then
+                Console.Write("{0} ", i)
+            End If
+        Next
+        Console.Read()
+    End Sub
+
+End Module
