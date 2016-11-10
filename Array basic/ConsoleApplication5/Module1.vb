296f1d16b0a60e4164d865166f534b0c4bf0dc20
DevonWenYan <devonproverb@gmail.com>
2016-11-10 16:51:08 +0800

    Array basic
    Array basic

---
+++ Array basic/ConsoleApplication5/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim RoleName() As String = {"魯夫", "喬巴", "羅賓", "香吉士", "騙人布"}
+        Dim money() As Integer = {100000, 10000, 1000, 100, 10}
+        Console.WriteLine("= =草帽海賊團的成員= =")
+        Console.WriteLine()
+        Console.WriteLine("姓名{0}選賞金額", vbTab)
+        Console.WriteLine("= = == = = = = = = == = =")
+        For i As Integer = 0 To RoleName.GetUpperBound(0)
+            Console.WriteLine("{0}{1}{2}", RoleName(i), vbTab, money(i))
+        Next
+        Console.Read()
+
+    End Sub
+
+End Module
